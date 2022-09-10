8119a93cc06b22d7701f3e39e53ab3f03bc10944
cindyodike <cindeyodike@gmail.com>
2022-09-10 14:05:30 -0700

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		printf("\n");
-
-		return (0);
-}
+#include <stdio.h> 
+  
+ /** 
+  *main -> assign a random number to the variable n each time it is executed 
+  *and print the last digit of the number stored in the variable n 
+  *Return: Always 0 (Success) 
+  */ 
+ int main(void) 
+ { 
+         int ch; 
+  
+         for (ch = 97; ch <= 122; ch++) 
+         { 
+                 putchar(ch); 
+         } 
+         putchar(10); /* this is an ascii code for new line*/ 
+  
+         return (0); 
+ }
